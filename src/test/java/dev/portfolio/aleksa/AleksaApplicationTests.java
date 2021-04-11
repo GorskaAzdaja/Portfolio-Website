d3a2f90f9acad104c4cc2535404873c545180915
GorskaAzdaja <alisancic@gmail.com>
2021-04-11 20:38:10 +0200

    Portfolio website commit

---
+++ src/test/java/dev/portfolio/aleksa/AleksaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.portfolio.aleksa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AleksaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
